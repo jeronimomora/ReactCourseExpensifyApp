25949c03cb40baf911424f8b63a5911b75e00a09
Jeronimo <jeronimora@berkeley.edu>
2019-03-24 17:16:15 -0700

    Fix typo

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,7 +6,7 @@ export const LoginPage = ({startLogin}) => (
 	<div className="box-layout">
 		<div className="box-layout__box">
 			<h1 className="box-layout__title">Expensify</h1>
-			<p>Please loging to begin or continue tracking expenses</p>
+			<p>Please log in to begin or continue tracking expenses</p>
 			<button
 				className="button" 
 				onClick={startLogin}>Login with Google</button>
